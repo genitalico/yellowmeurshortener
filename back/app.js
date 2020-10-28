136d7215117041082b30fe009722948b56399579
Galileo Teco <g@80bits.com>
2020-10-28 17:16:22 -0600

    mongodb added on reqs

--- back/app.js
+++ back/app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var connection = require("./mongodb/connection");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(connection.connection(app, {}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
